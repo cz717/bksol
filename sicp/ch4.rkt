fcf44f10a5115d087ba43d59add2fb181c55640f
cz717 <huajianye630@gmail.com>
2015-11-26 20:23:41 +0800

    sicp 4.22 - 4.44

--- sicp/ch4.rkt
+++ sicp/ch4.rkt
@@ -196,3 +196,104 @@
      (if (= n 0)
          #f
          (ev? ev? od? (- n 1))))))
+
+
+;; Exercise 4.22 - 4.26
+;  Admit.
+
+
+;; Exercise 4.27
+;  2
+;  10
+;  2
+
+
+;; Exercise 4.28
+;  Procedure as argument. Because all arguments will be delay,
+;  they must be force when they are called in the body.
+
+
+;; Exercise 4.29 - 4.31
+;  Abort.
+
+
+;; Exercise 4.32 - 4.34
+;  Admit.
+
+
+;; Exercise 4.35
+(define (an-integer-between low high)
+  (if (> low high)
+      (amb)
+      (amb low (an-integer-between (+ low 1) high))))
+
+
+;; Exercise 4.36
+(define (all-pythagorean-triple)
+  (let ((k (an-integer-starting-from 1)))
+    (let ((j (an-integer-between 1 k)))
+      (let ((i (an-integer-between 1 j)))
+        (require (= (+ (* i i) (* j j)) (* k k)))
+        (list i j k)))))
+
+
+;; Exercise 4.37
+;  Yes.
+
+
+;; Exercise 4.38 - 4.39
+;  Admit.
+
+
+;; Exercise 4.40
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 ))
+        (cooper (amb 2 3 4 5))
+        (fletcher (amb 2 3 4))
+        (smith (amb 1 2 3 4 5)))
+    (let ((miller (an-integer-between (+ cooper 1) 5)))
+      (require
+        (distinct? (list baker cooper fletcher miller smith)))
+      (require (not (= (abs (- smith fletcher)) 1)))
+      (require (not (= (abs (- fletcher cooper)) 1)))
+      (list (list 'baker baker)
+            (list 'cooper cooper)
+            (list 'fletcher fletcher)
+            (list 'miller miller)
+            (list 'smith smith)))))
+
+
+;; Exercise 4.41
+;  Admit.
+
+
+;; Exercise 4.4
+(define (xor p q)
+  (not (eq? p q)))
+
+(define (liar-puzzle)
+  (let ((betty (amb 1 2 3 4 5))
+        (ethel (amb 1 2 3 4 5))
+        (joan (amb 1 2 3 4 5))
+        (kitty (amb 1 2 3 4 5))
+        (mary (amb 1 2 3 4 5)))
+    (require (xor (= kitty 2)
+                  (= betty 3)))
+    (require (xor (= ethel 1)
+                  (= joan 2)))
+    (require (xor (= joan 3)
+                  (= ethel 5)))
+    (require (xor (= kitty 2)
+                  (= mary 4)))
+    (require (xor (= mary 4)
+                  (= betty 1)))
+    (list (list 'betty betty)
+          (list 'ethel ethel)
+          (list 'joan joan)
+          (list 'kitty kitty)
+          (list 'mary mary))))
+             
+
+
+;; Exercise 4.43 - 4.44
+;  Admit.
