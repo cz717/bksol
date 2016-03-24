d92221c1487bfd8d52257624e30eb8c6790d094e
cz717 <huajianye630@gmail.com>
2016-03-24 13:29:59 +0800

    coqart ch5

--- coqart/ch5.v
+++ coqart/ch5.v
@@ -1,3 +1,4 @@
+
 (** ** Chapter 5. Everyday Logic *)
 
 (** Exercise 5.4 *)
@@ -144,3 +145,141 @@ Qed.
 (** [] *)
 
 
+(** Exercise 5.13 *)
+
+Definition my_False : Prop := forall P : Prop, P.
+Definition my_not (P : Prop) : Prop := P -> my_False.
+
+Theorem nf : my_not my_False.
+Proof.
+  unfold my_False, my_not.
+  trivial.
+Qed.
+
+Theorem offset : forall P : Prop, my_not (my_not (my_not P)) -> my_not P.
+Proof.
+  unfold my_False, my_not. auto.
+Qed.
+
+Theorem ex3 : forall P Q : Prop,  my_not (my_not (my_not P)) -> P -> Q.
+Proof.
+  intros P Q H HP.
+  apply (offset P H HP).
+Qed.
+
+Theorem ex4 : forall P Q : Prop, (P -> Q) -> my_not Q -> my_not P.
+Proof.
+  unfold my_not. intros P Q PQ NQ HP.
+  apply NQ. apply PQ. assumption.
+Qed.
+
+Theorem ex5 : forall P Q R : Prop, (P -> Q) -> (P -> my_not Q) -> P -> R.
+Proof.
+  unfold my_not. intros P Q R PQ PNQ HP.
+  apply (PNQ HP (PQ HP)).
+Qed.
+
+(** [] *)
+
+
+Section leibniz.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Variable A : Set.
+
+Definition leibniz (a b : A) : Prop :=
+  forall (P : A -> Prop), P a -> P b.
+
+Require Import Relations.
+
+Theorem leibniz_sym : symmetric A leibniz.
+Proof.
+  unfold symmetric, leibniz.
+  intros x y H P.
+  apply H. trivial.
+Qed.
+
+
+(** Exercise 5.14 *)
+
+Theorem leibniz_refl : reflexive A leibniz.
+Proof.
+  unfold reflexive, leibniz. trivial.
+Qed.
+
+Theorem leibniz_trans : transitive A leibniz.
+Proof.
+  unfold transitive, leibniz.
+  intros x y z xy yz.
+  apply yz. apply xy.
+Qed.
+
+Theorem leibniz_equiv : equiv A leibniz.
+Proof.
+  unfold equiv. repeat split.
+  exact leibniz_refl.
+  exact leibniz_trans.
+  exact leibniz_sym.
+Qed.
+
+Theorem leibniz_least_reflexive : forall R:relation A, 
+  reflexive A R -> inclusion A leibniz R.
+Proof.
+  unfold inclusion, reflexive, leibniz.
+  intros R RA x y E.
+  apply E. apply RA.
+Qed.
+
+Theorem leibniz_eq : forall a b:A, leibniz a b -> a = b.
+Proof.
+  unfold leibniz. intros a b E.
+  apply E. reflexivity.
+Qed.
+
+Theorem eq_leibniz : forall a b:A, a = b -> leibniz a b.
+Proof.
+  unfold leibniz. intros a b E P PA.
+  rewrite <- E. assumption.
+Qed.
+
+Theorem leibniz_ind : forall (x : A) (P : A -> Prop), 
+  P x -> forall y : A, leibniz x y -> P y.
+Proof.
+  unfold leibniz. intros x P PX y E.
+  apply E. assumption.
+Qed.
+
+Unset Implicit Arguments.
+End leibniz.
+
+(** [] *)
+
+
+Definition my_le (n p : nat) : Prop :=
+  forall P : nat -> Prop, P n -> 
+    (forall q : nat, P q -> P (S q)) -> P p.
+
+(** Exercise 5.16 *)
+
+Lemma my_le_n : forall n : nat, my_le n n.
+Proof.
+  unfold my_le. intros n P Pn H.
+  assumption.
+Qed.
+
+Lemma my_le_S : forall n p : nat, my_le n p -> my_le n (S p).
+Proof.
+  unfold my_le.
+  intros n p NP P Pn H.
+  apply H. apply NP; assumption.
+Qed.
+
+Lemma my_le_le : forall n p : nat, my_le n p -> n <= p.
+Proof.
+  unfold my_le. intros n p H.
+  apply H.
+  - apply le_n.
+  - apply le_S.
+Qed.
+
+(** [] *)
